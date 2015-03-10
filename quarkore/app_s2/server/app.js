18e38726e87957ee1122399057cfcd470ed2e027
Deogracias Maroto <deogracias.maroto@gmail.com>
2015-03-10 10:33:03 +0000

    dia 2

---
+++ quarkore/app_s2/server/app.js
@@ -0,0 +1,15 @@
+var server = require('server');
+var router = require('router');
+var fs = require("fs");
+var handler = require("request_handler");
+var JSON_paths = {};
+
+
+fs.readFile('routes.json', 'utf-8', function(error, data){
+    if(error){
+        console.log('error');
+        throw error;
+    }
+    JSON_paths = data;
+});
+server.start(router.route, handler.handle, JSON_paths);
